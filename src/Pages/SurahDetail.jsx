ec1594653d486d8753cfde5189c6f2d2737824c9
ilhamdika <ilham27dika@gmail.com>
2024-10-16 13:46:17 +0700

    feat: add surah in detail

--- src/Pages/SurahDetail.jsx
+++ src/Pages/SurahDetail.jsx
@@ -27,7 +27,7 @@ const SurahDetail = () => {
     const ayat = detailSurah.ayat;
 
     // console.log(ayat);
-    console.log(detailSurah);
+    // console.log(detailSurah);
 
   return (
    <>
@@ -41,10 +41,11 @@ const SurahDetail = () => {
                 <button onClick={() => navigate(-1)}>
                     <FaArrowLeft />
                 </button>
-                <div className="grid grid-cols-1 dekstop:h-screen laptop:h-screen laptop:flex-row dekstop:flex-row tablet:flex-row mobile:h-auto mobile:grid-cols-1 pt-10 mobile:pt-32">
+                <div className="grid grid-cols-1 lg:h-screen xl:h-screen lg:flex-row dekstop:flex-row md:flex-row sm:h-auto sm:grid-cols-1 pt-10 sm:pt-32">
                     <div className="items-right">
-                        <h1 className="text-3xl font-bold dark:text-white">{detailSurah.namaLatin}</h1>
+                        <h1 className="text-3xl font-bold dark:text-white">{detailSurah.namaLatin}({detailSurah.arti})</h1>
                         <h1 className="text-3xl font-bold dark:text-white">{detailSurah.nama}</h1>
+                        <h5 className="text-xl font-bold dark:text-white">Jumlah Ayat: {detailSurah.jumlahAyat}</h5>
                         <p className="dark:text-white">{<span dangerouslySetInnerHTML={{ __html: detailSurah.deskripsi }} />}</p>
                         <div className="mt-4">
                             <h5 className="dark:text-white font-light">Dengarkan Surat:</h5>
@@ -54,6 +55,21 @@ const SurahDetail = () => {
                             </audio>
                         </div>
                     </div>
+                    {ayat.map((item, index) => (
+                        <div className="border-2 border-slate-300 mt-10" key={index}>
+                            <div className='text-right p-2'>
+                                <div className='flex justify-end'>
+                                    <h5 className="dark:text-white">{item.teksArab}</h5>
+                                </div>
+                            </div>
+                            <div className='text-left p-2'>
+                                <h5 className="dark:text-white font-light">{item.teksLatin}</h5>
+                            </div>
+                            <div className='text-left p-2'>
+                                <h5 className="dark:text-white font-light">Arti: {item.teksIndonesia}</h5>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
         ) : (
