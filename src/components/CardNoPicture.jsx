abbdaf28071e5c4b296d49208a87e1fd422cfca1
ilhamdika <ilham27dika@gmail.com>
2024-10-16 13:45:47 +0700

    fix: change fit

--- src/components/CardNoPicture.jsx
+++ src/components/CardNoPicture.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export const CardNoPicture = ({className, title, description, href, target, surah, arabText, audio}) => {
   return (
-    <div className={`mobile:h-96 mobile:w-full mobile:my-8 border-2 border-slate-300 mt-10` + className }>
+    <div className={`sm:h-96 sm:w-full sm:my-8 border-2 border-slate-300 mt-10` + className }>
       <div>
         <Link to={href} target={target}>
           <h3 className="text-3xl mb-2 dark:text-white hover:text-blue-500">
@@ -20,13 +20,13 @@ export const CardNoPicture = ({className, title, description, href, target, sura
               {description}
           </p>
 
-          <div className="mt-4 text-center">
+          {/* <div className="mt-4 text-center">
             <h5 className="dark:text-white font-light text-center">Dengarkan Surat:</h5>
             <audio controls className="w-full">
               <source src={audio} type="audio/mpeg" />
               Your browser does not support the audio element.
             </audio>
-          </div>
+          </div> */}
       </div>
     </div>
   )
